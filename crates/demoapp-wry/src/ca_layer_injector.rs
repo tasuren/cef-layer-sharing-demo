e4e605c17b438206bad95eddd0da89a817f6b50f
Takagi Tasuku <tasuren@outlook.jp>
2026-01-05 14:17:59 +0900

    fix: correct variable name with class name in demoapp-wry

--- crates/demoapp-wry/src/ca_layer_injector.rs
+++ crates/demoapp-wry/src/ca_layer_injector.rs
@@ -8,21 +8,21 @@ pub fn get_view(window: &Window) -> Option<Retained<NSView>> {
 }
 
 pub fn inject_ca_layer_host(context_id: ContextId, window: &Window) {
-    let tao_view = get_view(window).unwrap();
-    let content_view = unsafe { tao_view.superview() }.unwrap(); // It should be `contentView` of `NSWindow`.
+    let wry_view = get_view(window).unwrap();
+    let content_view = unsafe { wry_view.superview() }.unwrap(); // It should be `contentView` of `NSWindow`.
 
     // Create new view for Chromium layer. We want to show wry content above.
     let mtm = MainThreadMarker::new().unwrap();
     let chromium_view = NSView::new(mtm);
 
-    chromium_view.setFrame(tao_view.frame());
+    chromium_view.setFrame(wry_view.frame());
     chromium_view.setWantsLayer(true);
 
     // Set up chromium layer.
     let chromium_layer = CALayerHost::init(CALayerHost::alloc());
     unsafe { chromium_layer.setContextId(context_id) };
 
-    chromium_layer.setFrame(tao_view.frame());
+    chromium_layer.setFrame(wry_view.frame());
     chromium_layer.setGeometryFlipped(true);
 
     chromium_view.layer().unwrap().addSublayer(&chromium_layer);
@@ -31,6 +31,6 @@ pub fn inject_ca_layer_host(context_id: ContextId, window: &Window) {
     content_view.addSubview_positioned_relativeTo(
         &chromium_view,
         NSWindowOrderingMode::Below,
-        Some(&tao_view),
+        Some(&wry_view),
     );
 }
